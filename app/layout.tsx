b51ef8d665e7f89fbcbec4a35ed1ad5f8ed7c0a3
truthdeal <barisaryacantepe@gmail.com>
2025-08-30 01:10:31 +0200

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -171,6 +171,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
